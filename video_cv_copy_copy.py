db896d7fe40e299e8704d6ba870528bfc6548fd1
Mateo Epalza <mateoepalzaramirez@gmail.com>
2021-01-07 01:05:25 -0500

    trying

--- video_cv_copy_copy.py
+++ video_cv_copy_copy.py
@@ -39,9 +39,11 @@ def define_classifier():
 def creates_mapping(gamma):
     # build a lookup table mapping the pixel values [0, 255] to
 	# their adjusted gamma values
+    
     invGamma = (1.0 / gamma)
     table = np.array([((i / 255.0) ** invGamma) * 255 for i in np.arange(0, 256)]).astype("uint8")
     return table
+    
 
 def detect(frame, classifier, model, mapping):
     # make a copy 
