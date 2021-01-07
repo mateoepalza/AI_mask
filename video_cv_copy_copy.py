af5ec06a631b8cd0a9dcf3c9aa464c70734d92b5
Mateo Epalza <mateoepalzaramirez@gmail.com>
2021-01-07 00:15:45 -0500

    gamma filter

--- video_cv_copy_copy.py
+++ video_cv_copy_copy.py
@@ -13,8 +13,10 @@ def main():
     classifier = define_classifier()
     # load the trained model
     model = load_model()
+    # creates a mapping pixel for the gamma values
+    mapping = creates_mapping(2)
     # capture the video 
-    video_capture(classifier, model)
+    video_capture(classifier, model, mapping)
 
 def load_model():
     # Get the path of the neural network
@@ -34,7 +36,16 @@ def define_classifier():
 
     return net
 
-def detect(frame, classifier, model):
+def creates_mapping(gamma):
+    #build a lookup table mapping the pixel values [0, 255] to
+	# their adjusted gamma values
+	invGamma = 1.0 / gamma
+	table = np.array([((i / 255.0) ** invGamma) * 255
+		for i in np.arange(0, 256)]).astype("uint8")
+	
+    return table
+
+def detect(frame, classifier, model, mapping):
     # make a copy 
     frame_copy = np.copy(frame)
     # obtain the width and height of the image
@@ -64,7 +75,7 @@ def detect(frame, classifier, model):
             face = frame_copy[startY:endY, startX:endX]
 
             # prepare the face
-            face = prep_face(face)
+            face = prep_face(face, mapping)
             # execute model
             res = execute_model(face, model) 
             # draw rectangle
@@ -95,9 +106,11 @@ def execute_model(face, model):
 
     return result
 
-def prep_face(face):
+def prep_face(face, mapping):
     # modify the width and height of the image
     image_f = cv2.resize(face, (width, height))
+    # Adjust the gamma of the image
+    image_f =  cv2.LUT(image_f, mapping)
     # change the color from RGB to grayscale
     image_f = cv2.cvtColor(image_f, cv2.COLOR_BGR2GRAY)
     # reshape
@@ -107,7 +120,16 @@ def prep_face(face):
 
     return image_f
 
-def video_capture(classifier, model):
+def adjust_gamma(image, gamma):
+    # build a lookup table mapping the pixel values [0, 255] to
+	# their adjusted gamma values
+	invGamma = 1.0 / gamma
+	table = np.array([((i / 255.0) ** invGamma) * 255
+		for i in np.arange(0, 256)]).astype("uint8")
+	# apply gamma correction using the lookup table
+    gamma = cv2.LUT(image, table)
+	return np.hstack([image, gamma])
+def video_capture(classifier, model, mapping):
     # the argument is the "position" of the camera
     cam = cv2.VideoCapture(0)
 
@@ -116,12 +138,12 @@ def video_capture(classifier, model):
         ret, frame =  cam.read()
 
         # In here we should do our operations
-        frame = detect(frame, classifier, model)
+        frame = detect(frame, classifier, model, mapping)
 
         # Display the resulting frame
         cv2.imshow('frame',frame)
 
-        #time.sleep()
+        time.sleep(0.025)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     
